69b69aadac46fd99a6fa006a5283b69b65069818
suibian <suibian.uvw@gmail.com>
2023-05-30 17:28:30 +0800

    [project 2] Permutation完成，得分96/100
    Compilation:  PASSED
    API:          PASSED
    
    SpotBugs:     PASSED
    PMD:          PASSED
    Checkstyle:   PASSED
    
    Correctness:  46/49 tests passed
    Memory:       51/51 tests passed
    Timing:       191/193 tests passed

--- queues/Permutation.java
+++ queues/Permutation.java
@@ -4,8 +4,33 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Iterator;
+
 public class Permutation {
     public static void main(String[] args) {
-        
+        if (args.length < 1) {
+            return;
+        }
+
+        int length = Integer.parseInt(args[0]);
+        RandomizedQueue<String> rq = new RandomizedQueue<String>();
+
+        while (!StdIn.isEmpty()) {
+            rq.enqueue(StdIn.readString());
+        }
+
+        Iterator<String> iter = rq.iterator();
+
+        for (int i = 0; i < length; i++) {
+            if (iter.hasNext()) {
+                StdOut.println(iter.next());
+            }
+            else {
+                break;
+            }
+        }
     }
 }
